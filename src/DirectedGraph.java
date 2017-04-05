2b094786f59e68db75464eb5062bbe23f83911c0
GlowBlood <ipberezovskaya@yandex.ru>
2017-04-06 01:16:06 +0300

    послание

--- src/DirectedGraph.java
+++ src/DirectedGraph.java
@@ -6,7 +6,8 @@ import java.util.HashMap;
 
 
 public class DirectedGraph {
-    final private class Node {}
+
+    final public class Node {}
 
     final public class Arrow {
         private int weight;
@@ -46,21 +47,31 @@ public class DirectedGraph {
 
 
     private final Map<String, Node> nodeMap = new HashMap<>();
+    public void nameNode (Node node){
+
+    }
 
     public void addNode(String name) {
-        if (nodeMap.containsKey(name)) throw new NoSuchElementException();
+        if (nodeMap.containsKey(name)) throw new IllegalArgumentException("Узел " + name + " уже существует");
         nodeMap.put(name, new Node());
+    }
+
+    public Node getNode(String name){
+        Node result = nodeMap.get(name);
+        if (result == null) throw new NoSuchElementException("Узла под именем " + name + " не существует");
+        return result;
     }
 
     public void removeNode(String name) {
         Node node = nodeMap.get(name);
-        if (getListInn(node) != null) throw new IllegalArgumentException("Удалите входящие в этот узел дуги");
-        if (getListOut(node) != null) throw new IllegalArgumentException("Удалите исходящие из этого узла дуги");
+        if (getListInn(node) != null) throw new IllegalArgumentException("Удалите входящие в узел под именем " + name + " дуги");
+        if (getListOut(node) != null) throw new IllegalArgumentException("Удалите исходящие из узла под именем " + name + " дуги");
         nodeMap.remove(name);
     }
 
     public void changeNameNode(String name, String newName) {
-        if (!nodeMap.containsKey(newName)) throw new NoSuchElementException();
+        if (!nodeMap.containsKey(name)) throw new NoSuchElementException("Узла под именем " + name + " не существует");
+        if (nodeMap.containsKey(newName)) throw new IllegalArgumentException("Узел под именем " + newName + " уже существует");
         Node node = nodeMap.get(name);
         nodeMap.remove(name);
         nodeMap.put(newName, node);
@@ -71,16 +82,16 @@ public class DirectedGraph {
 
 
     public void addArrow(Arrow arrow, String nameStart, String nameFinish){
+        ArrayList<Arrow> arrayStart = new ArrayList<>();
         final Node in = nodeMap.get(nameStart);
         if (in == null) throw new NoSuchElementException("Не существует начального узла " + nameStart);
-        ArrayList<Arrow> arrayStart = incomingArrows.get(in);
-        arrayStart.add(arrow);
+        arrayStart = incomingArrows.get(in);
 
         final Node out = nodeMap.get(nameFinish);
         if (out == null) throw new NoSuchElementException("Не существует конечного узла " + nameFinish);
         ArrayList<Arrow> arrayFinish = outcomingArrows.get(out);
 
-        if (arrayStart.contains(arrow) && (arrayFinish.contains(arrow))) throw new IllegalArgumentException("Дуга " + arrow + " уже существует");
+        if (arrayStart.contains(arrow) && (arrayFinish.contains(arrow))) throw new IllegalArgumentException("Дуга между узлами " + nameStart + "и " + nameFinish + " уже существует");
 
         arrayStart.add(arrow);
         arrayFinish.add(arrow);
@@ -101,23 +112,41 @@ public class DirectedGraph {
     public ArrayList<Arrow> getListInn(Node node){
         final ArrayList<Arrow> in = incomingArrows.get(node);
         if (in == null) throw new NoSuchElementException("Узла " + node + " не существует");
-        return incomingArrows.get(node);
+        return in;
     }
 
     public ArrayList<Arrow> getListOut(Node node){
         final ArrayList<Arrow> out = outcomingArrows.get(node);
         if (out == null) throw new NoSuchElementException("Узла " + node + " не существует");
-        return outcomingArrows.get(node);
+        return out;
+    }
+
+    private ArrayList listOfNode(String key){
+        ArrayList result = new ArrayList<>();
+        result.add("имя узла " + key);
+        Node i = getNode(key);
+        result.add("Список исходящих дуг");
+        result.add(getListOut(i));
+        result.add("Список входящих дуг");
+        result.add(getListInn(i));
+        return result;
     }
 
 @Override
 public int hashCode(){
-        return 2 * incomingArrows.hashCode();
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + incomingArrows.hashCode();
+    return result;
 }
 
 @Override
 public String toString() {
-    return nodeMap.toString();
+    ArrayList newResult = new ArrayList<>();
+    for (String key : nodeMap.keySet()) {
+        newResult.add(listOfNode(key));
+    }
+return newResult.toString();
 }
 
 @Override
